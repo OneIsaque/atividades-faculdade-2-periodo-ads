8db8304b3a515e89a904a4bfb05f33bad2dfa105
Isaque João P. <124298767+OneIsaque@users.noreply.github.com>
2025-09-08 21:03:26 -0300

    feat: docs: add pasta POO

---
+++ POO/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        try {
+           
+        } catch (ArithmeticException e) {
+           
+        } finally {
+            System.out.println("Programa finalizado");
+        }
+    }
+}
